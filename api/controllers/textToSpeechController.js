1c27d59f484aab50f1b4bf65f2a75bb5a793decb
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-03-07 12:43:27 +0530

    working output generated

--- api/controllers/textToSpeechController.js
+++ api/controllers/textToSpeechController.js
@@ -27,9 +27,12 @@ exports.convert = function (req, res) {
 
         // Performs the text-to-speech request
         const [response] = await client.synthesizeSpeech(request);
+
         // Write the binary audio content to a local file
         const writeFile = util.promisify(fs.writeFile);
+
         await writeFile('output.mp3', response.audioContent, 'binary');
+
         console.log('Audio content written to file: output.mp3');
     }
     quickStart();
