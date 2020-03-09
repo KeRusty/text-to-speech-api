04cd17532f4952e97f6941577f5645b7937f3a95
Chathu Abeywickrama <chathu.git93@gmail.com>
2020-03-09 07:40:49 +0530

    changes to function and readme updated

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+## Obtain authentication credentials.
+
+    Create local credentials by running the following command
+
+    * Go to API Manager -> Credentials
+    * Click "New Credentials", and create a service account or [click here](https://console.cloud.google.com/project/_/apiui/credential/serviceaccount)
+    * Download the JSON for this service account, and set the `GOOGLE_APPLICATION_CREDENTIALS`
+    environment variable to point to the file containing the JSON credentials.
+
+    Set the `GOOGLE_APPLICATION_CREDENTIALS` environment variable:
+
+    Linux:
+
+        export GOOGLE_APPLICATION_CREDENTIALS=/path/to/service_account_file.json
+
+    Windows:
+
+        set GOOGLE_APPLICATION_CREDENTIALS=/path/to/service_account_file.json
+
+    Windows (PowerShell):
+
+        $env:GOOGLE_APPLICATION_CREDENTIALS="/path/to/service_account_file.json"
+
+
+
 ### `npm install`
 
 This will install the app<br />
@@ -8,3 +33,5 @@ Runs the app in the development mode.<br />
 
 It will run at [http://localhost:3001](http://localhost:3001)
 <br />
+
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const textToSpeech = require('@google-cloud/text-to-speech');
+const speech = require('@google-cloud/speech');
 const fs = require('fs');
 const util = require('util');
 const cors = require('cors');
@@ -21,7 +22,7 @@ app.post('/ttsConvert', (req, res) => {
 
     const client = new textToSpeech.TextToSpeechClient();
     // output the book to the console for debugging
-    async function quickStart() {
+    async function textToSpeech() {
 
         // The text to synthesize
         // const text = 'hello, world!';
@@ -45,9 +46,56 @@ app.post('/ttsConvert', (req, res) => {
 
         //console.log('Audio content written to file: output.mp3');
     }
-    quickStart();
+    textToSpeech();
 
-    res.send('Audio content written to file: output.mp3');
+    res.send('Audio content written to file on your Server Folder: output.mp3');
 });
+
+app.post('/SpeechConvert', (req, res) => {
+    const data = req.body;
+
+    console.log(data)
+
+    const client = new speech.SpeechClient();
+
+    async function speechToText() {
+
+        // The name of the audio file to transcribe
+        const fileName = data;
+
+        // Reads a local audio file and converts it to base64
+        const file = fs.readFileSync(fileName);
+        const audioBytes = file.toString('base64');
+
+        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+        const audio = {
+            content: audioBytes,
+        };
+        const config = {
+            encoding: 'LINEAR16',
+            sampleRateHertz: 16000,
+            languageCode: 'en-US',
+        };
+        const request = {
+            audio: audio,
+            config: config,
+        };
+
+        // Detects speech in the audio file
+        const [response] = await client.recognize(request);
+        const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+        console.log(`Transcription: ${transcription}`);
+    }
+    speechToText().catch(console.error);
+
+    res.send('Transcribed Successfully');
+
+
+})
+
+
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
