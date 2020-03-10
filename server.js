772717c8a06bd676c85597a4b0126d55f049db24
GihanRangana <itsmerangana@gmail.com>
2020-03-10 11:14:22 +0530

    errors fixed on server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 3000
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
@@ -23,8 +23,8 @@ app.post('/ttsConvert', (req, res) => {
     const data = req.body;
 
 
-
     const client = new textToSpeech.TextToSpeechClient();
+
     // output the book to the console for debugging
     async function textToSpeechConverter() {
 
@@ -33,11 +33,11 @@ app.post('/ttsConvert', (req, res) => {
 
         // Construct the request
         const request = {
-            input: { text: data.text },
+            input: {text: data.text},
             // Select the language and SSML voice gender (optional)
-            voice: { languageCode: data.language, ssmlGender: data.gender },
+            voice: {languageCode: data.language, ssmlGender: data.gender},
             // select the type of audio encoding
-            audioConfig: { audioEncoding: 'MP3' },
+            audioConfig: {audioEncoding: 'MP3'},
         };
 
         // Performs the text-to-speech request
@@ -50,6 +50,7 @@ app.post('/ttsConvert', (req, res) => {
 
         //console.log('Audio content written to file: output.mp3');
     }
+
     textToSpeechConverter();
     // res.download();
     res.send('Audio content written to file on your Server Folder: output.mp3');
@@ -94,15 +95,12 @@ app.post('/SpeechConvert', (req, res) => {
             // .map(result => result)
             .join('\n');
         console.log(`Transcription: ${transcription}`);
+        res.send(transcription);
     }
+
     speechToText().catch(console.error);
 
-    res.send('Transcribed Successfully');
-
-
-})
-
-
+});
 
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
